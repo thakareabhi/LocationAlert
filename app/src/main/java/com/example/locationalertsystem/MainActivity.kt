a9b557fb6ff99d41b11b274c516134945555812f
ABHINAV THAKARE <thakareabhi7@gmail.com>
2021-02-07 15:02:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/locationalertsystem/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.locationalertsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val showButton = findViewById<Button>(R.id.button)
+
+        // finding the edit text
+        val editText = findViewById<EditText> (R.id.editText)
+
+        val rootRef: DatabaseReference = FirebaseDatabase.getInstance().reference
+        val demoRef: DatabaseReference = rootRef.child("Vibrate").child("LocationVar")
+
+        showButton.setOnClickListener() {
+            var str=editText.text.toString();
+            if(str.equals(""))
+            { }
+            else
+            {
+                demoRef.setValue(str)
+                Toast.makeText(applicationContext,"Done",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+    }
+}
